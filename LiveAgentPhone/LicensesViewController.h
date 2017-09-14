b0a6ccf2b1ca7b375b483b35e1afe714e454d5b6
Rastislav Kostrab <r.kostrab@gmail.com>
2017-09-14 19:51:00 +0200

    Licenses

---
+++ LiveAgentPhone/LicensesViewController.h
@@ -0,0 +1,13 @@
+//
+//  LicensesViewController.h
+//  LiveAgentPhone
+//
+//  Created by Rastislav Kostrab on 14.9.17.
+//  Copyright © 2017 Quality Unit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LicensesViewController : UIViewController
+
+@end
---
+++ LiveAgentPhone/LicensesViewController.m
@@ -0,0 +1,40 @@
+//
+//  LicensesViewController.m
+//  LiveAgentPhone
+//
+//  Created by Rastislav Kostrab on 14.9.17.
+//  Copyright © 2017 Quality Unit. All rights reserved.
+//
+
+#import "LicensesViewController.h"
+
+@interface LicensesViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
+
+@implementation LicensesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSString *htmlFile = [[NSBundle mainBundle] pathForResource:@"licenses" ofType:@"html"];
+    NSString* htmlString = [NSString stringWithContentsOfFile:htmlFile encoding:NSUTF8StringEncoding error:nil];
+    [[self webView] loadHTMLString:htmlString baseURL: [[NSBundle mainBundle] bundleURL]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
