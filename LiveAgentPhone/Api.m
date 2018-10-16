5daf178dcb6d2145fbb9f328868dfbb4923ec9e3
Rastislav Kostrab <r.kostrab@gmail.com>
2018-10-16 09:03:07 +0200

    create mobile app device if not exist

--- LiveAgentPhone/Api.m
+++ LiveAgentPhone/Api.m
@@ -64,6 +64,7 @@
         NSMutableDictionary *filters = [[NSMutableDictionary alloc] init];
         NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
         NSString *phoneId = [userDefaults objectForKey:memoryKeySipId];
+        NSString *agentId = [userDefaults objectForKey:memoryKeyAgentId];
         if (phoneId == nil) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 failure(@"Variable 'phoneId' not found on this device");
@@ -90,16 +91,31 @@
         [manager GET:@"devices" parameters:requestParameters progress:nil success:^(NSURLSessionTask *task, id responseObject) {
             if (responseObject != nil && [responseObject isKindOfClass:[NSArray class]]) {
                 NSArray *response = responseObject;
-                if ([response count] == 0) {
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        failure(@"No phone services found.");
-                    });
-                    return;
-                }
                 NSLog(@"SUCCESS '%@'", requestDescription);
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    success(responseObject);
-                });
+                // check if mobile device exists (if no then create it)
+                BOOL isMobileDevice = NO;
+                if (response != nil && [response count] > 0) {
+                    for (NSDictionary* device in response) {
+                        if ([[device objectForKey:@"type"] isEqualToString:@"A"]) {
+                            isMobileDevice = YES;
+                            break;
+                        }
+                    }
+                }
+                if (!isMobileDevice) {
+                    [self createDeviceWithPhoneId:phoneId agentId:agentId success:^(NSDictionary *mobileDevice) {
+                        // mobile device successfuly created here and add it to the devices array
+                        NSMutableArray* mutableDevices = [NSMutableArray arrayWithArray:response];
+                        [mutableDevices addObject:mobileDevice];
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            success(mutableDevices);
+                        });
+                    } failure:failure];
+                } else {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        success(responseObject);
+                    });
+                }
             } else {
                 NSString *errorMessage = errorMsgCannotParseResponse;
                 NSLog(@"FAILURE '%@' - %@", requestDescription, errorMessage);
@@ -111,6 +127,51 @@
             NSHTTPURLResponse *resp = (NSHTTPURLResponse *) [operation response];
             [self deviceFailure:requestDescription error:error httpCode:[resp statusCode] failure:failure];
         }];
+    });
+}
+
++(void)createDeviceWithPhoneId:(NSString*)phoneId agentId:(NSString*)agentId success:(void (^)(NSDictionary *mobileDevice))success failure:(void (^)(NSString *errorMessage))failure {
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul);
+    dispatch_async(queue, ^{
+        // build body
+        NSMutableDictionary *body = [[NSMutableDictionary alloc] init];
+        [body setObject:phoneId forKey:@"phone_id"];
+        [body setObject:agentId forKey:@"agent_id"];
+        [body setObject:@"A" forKey:@"type"];
+        [body setObject:@"P" forKey:@"service_type"];
+        [body setObject:@"N" forKey:@"status"];
+        // build request
+        AFHTTPSessionManager *manager = [Net createSessionManager];
+        NSError *error;
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:body options:0 error:&error];
+        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@/devices", [manager baseURL]]] cachePolicy:NSURLRequestReloadIgnoringCacheData  timeoutInterval:timeoutSec];
+        [request setHTTPMethod:@"POST"];
+        [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+        [request setValue:[Net getApikey] forHTTPHeaderField:@"apikey"];
+        [request setHTTPBody:[jsonString dataUsingEncoding:NSUTF8StringEncoding]];
+        NSString *requestDescription = @"POST /devices";
+        NSLog(@"%@", requestDescription);
+        [[manager dataTaskWithRequest:request completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
+            if (!error) {
+                if (responseObject != nil && [responseObject isKindOfClass:[NSDictionary class]]) {
+                    NSLog(@"SUCCESS '%@'", requestDescription);
+                    success(responseObject); // dispatch on the same thread
+                } else {
+                    NSString *errorMessage = errorMsgCannotParseResponse;
+                    NSLog(@"FAILURE '%@' - %@", requestDescription, errorMessage);
+                    failure(errorMessage); // dispatch on the same thread
+                }
+            } else {
+                //                NSHTTPURLResponse *resp = (NSHTTPURLResponse *) response;
+                //                [self deviceFailure:requestDescription error:error httpCode:[resp statusCode] failure:failure];
+                NSString *errorMessage = [error localizedDescription];
+                NSLog(@"FAILURE '%@' - %@", requestDescription, errorMessage);
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    failure(errorMessage);
+                });
+            }
+        }] resume];
     });
 }
 
